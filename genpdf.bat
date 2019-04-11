cc0b5e61f3a299595aded380e651beba4ca34a08
wellsst <wellsst@gmail.com>
2019-04-11 17:00:44 +1000

    PDF rendered by Chrome plugins
    Use the Chrome plugins:
    * to produce similar to the webpage use PDF Mage, settings portrait, single page, 1024
    * a simple PDF use: https://chrome.google.com/webstore/detail/print-friendly-pdf/ohlencieiipommannpdfcmfdpjjmeolj?hl=en

--- genpdf.bat
+++ genpdf.bat
@@ -1,3 +1,8 @@
+rem Dont use this script its not qorking well enough
+rem Use the Chrome plugins:
+rem to produce similar to the webpage use PDF Mage, settings portrait, single page, 1024
+rem a simple PDF use: https://chrome.google.com/webstore/detail/print-friendly-pdf/ohlencieiipommannpdfcmfdpjjmeolj?hl=en
+
 wkhtmltopdf  http://localhost:4000/online-cv/ assets/pdf/steven_wells_resume.pdf 
 pause
 rem --disable-smart-shrinking
