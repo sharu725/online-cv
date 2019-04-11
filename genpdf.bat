4d48833f00abeaf59a4c9e73a2f04f44e6033bf8
wellsst <wellsst@gmail.com>
2019-04-11 15:26:37 +1000

    Convert to PDF and formatting

---
+++ genpdf.bat
@@ -0,0 +1,5 @@
+wkhtmltopdf  http://localhost:4000/online-cv/ assets/pdf/steven_wells_resume.pdf 
+pause
+rem --disable-smart-shrinking
+rem --viewport-size 1024x768 
+rem --margin-left 0mm --margin-right 0mm --margin-top 27mm --margin-bottom 12mm
