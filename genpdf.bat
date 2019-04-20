dc52d1a0ba16627a411bacb9fdaed926b1f2cae7
wellsst <wellsst@gmail.com>
2019-04-20 15:34:12 +1000

    Update pdf

--- genpdf.bat
+++ genpdf.bat
@@ -3,7 +3,8 @@ rem Use the Chrome plugins:
 rem to produce similar to the webpage use PDF Mage, settings portrait, single page, 1024
 rem a simple PDF use: https://chrome.google.com/webstore/detail/print-friendly-pdf/ohlencieiipommannpdfcmfdpjjmeolj?hl=en
 
-wkhtmltopdf  http://localhost:4000/online-cv/ assets/pdf/steven_wells_resume.pdf 
+wkhtmltopdf -d 300 --viewport-size 1024x768 --margin-left 0mm --margin-right 0mm --margin-top 0mm --margin-bottom 0mm https://wellsst.github.io/online-cv/ assets/pdf/steven_wells_resume.pdf 
+rem wkhtmltopdf  http://localhost:4000/online-cv/ assets/pdf/steven_wells_resume.pdf 
 pause
 rem --disable-smart-shrinking
 rem --viewport-size 1024x768 
