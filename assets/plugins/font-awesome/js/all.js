e299c6c5a13cbfc4403d4127b744c0beba2b15e9
William Almeida <will.allmeida@gmail.com>
2020-07-21 10:54:40 -0300

    Minor updates

--- assets/plugins/font-awesome/js/all.js
+++ assets/plugins/font-awesome/js/all.js
@@ -2201,7 +2201,7 @@ function makeInlineSvgAbstract(params) {
       'data-icon': iconName,
       'class': attrClass,
       'role': 'img',
-      'xmlns': 'http://www.w3.org/2000/svg',
+      'xmlns': 'https://www.w3.org/2000/svg',
       'viewBox': '0 0 ' + width + ' ' + height
     })
   };
